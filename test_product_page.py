bc2557e8e2dbd7bd494ca000d34c3b2999e04670
MVGIC <ex3ct@mail.ru>
2023-03-25 11:12:17 +0300

    new tests added

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 import pytest
 
+from pages.basket_page import BasketPage
 from pages.login_page import LoginPage
 from pages.product_page import ProductPage
 
@@ -70,4 +71,16 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page.should_be_login_page()
 
 
-#TODO:  4) Сделать коммит.
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    basket_page = BasketPage(browser, link, 4)
+    basket_page.should_not_be_product_in_basket()
+    basket_page.should_be_empty_basket_message()
+
+# Гость открывает страницу товара
+# Переходит в корзину по кнопке в шапке
+# Ожидаем, что в корзине нет товаров
+# Ожидаем, что есть текст о том что корзина пуста
